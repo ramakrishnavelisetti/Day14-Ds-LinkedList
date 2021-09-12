2b042ad65f810ed610bc0af48181b39b6eef8751
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-12 12:50:59 +0530

    Added: Append Data At End

--- src/main/java/com/bdlz/DSLS/LinkedListMain.java
+++ src/main/java/com/bdlz/DSLS/LinkedListMain.java
@@ -7,9 +7,14 @@ public class LinkedListMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to add the data at the start");
-        switch(scanner.nextInt()){
+        System.out.println("Enter 2 to add the data at the end");
+        switch(scanner.nextInt()) {
             case 1:
                 Operations.addDataAtStart();
+                break;
+            case 2:
+                Operations.addDataAtEnd();
+                break;
         }
     }
 }
--- src/main/java/com/bdlz/DSLS/Operations.java
+++ src/main/java/com/bdlz/DSLS/Operations.java
@@ -2,12 +2,17 @@ package com.bdlz.DSLS;
 
 public class Operations {
     public static void addDataAtStart(){
-        LinkedList linkedList= new LinkedList();
+        LinkedList linkedList = new LinkedList();
         linkedList.push(70);
         linkedList.push(30);
         linkedList.push(56);
         linkedList.print();
-
-
+    }
+    public static void addDataAtEnd() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
+        linkedList.print();
     }
 }
