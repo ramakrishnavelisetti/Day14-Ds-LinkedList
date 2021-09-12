75e0115c15d377edb59825c48475ad38a562f7f7
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-12 12:35:28 +0530

    Added: Adding Data At Start

--- src/main/java/com/bdlz/DSLS/LinkedListMain.java
+++ src/main/java/com/bdlz/DSLS/LinkedListMain.java
@@ -1,20 +1,15 @@
 package com.bdlz.DSLS;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
 
     public static void main(String[] args) {
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        Node head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node tail = thirdNode;
-
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start");
+        switch(scanner.nextInt()){
+            case 1:
+                Operations.addDataAtStart();
         }
     }
 }
